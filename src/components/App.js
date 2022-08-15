ea3de9d4db5381ebce7fd85ce636c328b7f9986b
Victor <mhpd100.i@gmail.com>
2022-08-15 13:07:20 +0300

    'React+Classes'

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+// import React from 'react';
+
+class Car {
+    color = 'red';
+    speed = 220;
+
+    go = () => {
+        console.log("GO");
+    };
+};
+
+console.log('Car', Car);
--- src/index.js
+++ src/index.js
@@ -1,27 +1,22 @@
-const mySort = (arr = [], func) => {
-    return () => { 
-      return arr.sort((value, next) => {
-        return typeof func === 'function'
-        ? func(value, next)
-        : value > next
-          ? 1
-          : value < next
-            ? -1
-            : 0;
-      });
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from './components/App.js';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//  <App />
+// );
+
+class Car {
+    color = 'red';
+    speed = 220;
+
+    go = () => {
+        console.log("GO");
     };
-  };
-  const arr = [ 1, 6, 15, 32 ];
-  
-  const sortFunc1 = mySort([...arr]);
-  const sortFunc2 = mySort([...arr], (value, next) => {
-    return value > next
-    ? -1
-    : value < next
-      ? 1
-      : 0;
-  });
-  
-  console.log('sortFunc', sortFunc1(), sortFunc2())
+};
 
+const car1 = new Car();
+const car2 = new Car();
 
+console.log('Car', car1, car2);
