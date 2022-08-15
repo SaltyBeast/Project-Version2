b7d286eda1f5483c02c0e0c83c4600b402d0fca2
Victor <mhpd100.i@gmail.com>
2022-08-15 14:02:56 +0300

    'React.Components'

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
 
 class App extends React.Component {
-    render = () => {
+    constructor (props) {
+        super(props);
         
+        this.color = 'red';
+
+    };
+
+    func = () => {
+        alert('Hallelujah!');
+    };
+
+    render = () => {
+        return <div onClick={this.func}>
+            {this.color}
+        </div>
     }; 
 }
 
--- src/index.js
+++ src/index.js
@@ -1,36 +1,11 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import App from './components/App.js';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './components/App.js';
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//  <App />
-// );
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+ <App />
+);
 
-class Parent {
-    constructor () {
-        console.log('constructor Parent');
-    };
 
-    render = () => {
-        console.log('render');
-    };
-};
-
-class Child extends Parent {
-    color = null;
-
-    constructor (color) {
-        super(color);
-
-        this.color = color;
-        this.render();
-    };
-
-    render = () => {
-        console.log('Child.render', this.color);
-    };
-};
-
-const a = new Child('red');
 
