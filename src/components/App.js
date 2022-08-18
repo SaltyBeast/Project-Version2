68b5ae83dab1b2b200ceddee2c0c6a96f09ea59c
Victor <mhpd100.i@gmail.com>
2022-08-18 21:58:58 +0300

    'ReactDom+NewEvents'

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,27 @@
 import React from 'react';
 
+const style = {
+    textAlign: 'center',
+    color: 'red',
+};
+const innerStyle = {
+    width: '50%',
+    margin: '0 auto',
+};
+
+const listener = (event) => {
+    console.log('onRedClick', event);
+};
+document.addEventListener('onRedClick', listener);
+
+setTimeout(() => {
+    document.dispatchEvent(new CustomEvent('onRedClick', {
+        'detail': {
+            hello: '123',
+        }
+    }));
+}, 3000);
+
 class App extends React.Component {
     constructor (props) {
         super(props);
@@ -8,15 +30,19 @@ class App extends React.Component {
 
     };
 
-    func = () => {
+    func = (e) => {
         alert('Hallelujah!');
     };
 
     render = () => {
-        return <div onClick={this.func}>
-            {this.color}
-        </div>
+        return <div 
+            style={style}
+            onClick={this.func}>
+                <div style={innerStyle}>
+                {this.color}
+                </div>
+        </div>;
     }; 
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './components/App.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
- <App />
+ <App onClick={() => console.log('asdasdsa')} />
 );
 
 
