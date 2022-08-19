577aa4f9ecda5f6c7753f572494aa3bb4da4a35b
WIN-JJV0OHJOAVK <user@WIN-JJV0OHJOAVK>
2022-08-19 18:51:25 +0300

    unction`

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 
+function Test () {
+    this[`color`] = `red`;
+    this[`length`] = 5;
+    
+    this[`go`] =  () =>  {
+        console.log(`------>`)
+    };
+} ;
+const a = new Test();
+
+console.log(`aaa`, a.go());
+
+
+
+
+
+
 
 
