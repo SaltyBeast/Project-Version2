9e4390c95f48f84ee4dd4a33751e5cd88596b5e7
Victor <mhpd100.i@gmail.com>
2022-08-20 19:54:27 +0300

    'DOM_Components'

--- src/routes/Account.jsx
+++ src/routes/Account.jsx
@@ -1,19 +1,40 @@
 import React from 'react';
 
+class Avatar extends React.Component {
+    render = () => {
+        return <img src='account.png' className='accountimg' />;
+    }; 
+};
 
+class Input1 extends React.Component {
+    render = () => {
+        return <input type='text' name='name' placeholder='Your name' className='accountinput' />;
+    }; 
+};
 
+class Input2 extends React.Component {
+    render = () => {
+        return <input type='text' name='nickname' placeholder='Your nickname' className='accountinput' />;
+    }; 
+};
+
+class SaveButton extends React.Component {
+    render = () => {
+        return <button className='accountbutton'>Save</button>;
+    }; 
+};
 
 class Account extends React.Component {
     render = () => {
-        return <div>
+        return <>
             {/* картинка, 2 поля, кнопка сохранить */}
-            <img src='account.png' id='accountimg' /><br/>
-            <div id='accountdiv'>
-                <input type='text' name='name' placeholder='Your name' id='accountinput' />
-                <input type='text' name='nickname' placeholder='Your nickname' id='accountinput' />
+            <Avatar /><br />
+            <div className='accountdiv'>
+                <Input1 />
+                <Input2 />
             </div><br />
-           <div id='accountspan'><button id='accountbutton'>Save</button></div>
-        </div>;
+           <div className='accountsave'><SaveButton /></div>
+        </>;
     }; 
 };
 
