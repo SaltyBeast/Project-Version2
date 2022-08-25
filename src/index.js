7173c9b54912ccbb8058d907b2ca99092482840a
WIN-JJV0OHJOAVK <user@WIN-JJV0OHJOAVK>
2022-08-25 11:57:47 +0300

    sorted

--- src/index.js
+++ src/index.js
@@ -1,20 +1,41 @@
 
-function Test () {
-    this[`color`] = `red`;
-    this[`length`] = 5;
-    
-    this[`go`] =  () =>  {
-        console.log(`------>`)
-    };
-} ;
-const a = new Test();
-
-console.log(`aaa`, a.go());
+const arr =  [
+    110,
+    1,
+    `hello world`,
+    3,
+    4,
+    18,
+    24 
+];
 
 
+const func = (prevValue, nextValue) => {
+    if (prevValue === nextValue) {
+        return 0;
+    }
+    if (prevValue < nextValue) {
+        return -1;
+    }
+    if (prevValue > nextValue) {
+        return 1;
+    }
+};
 
+const sortNumbers = arr
+.filter((value) => {
+    if (typeof value === 'number') {
+        return true;
+    }
+    return false;
+})
+.sort(func);
 
+const word = arr.filter((value) => {
+    if (typeof value === 'string') {
+    return true;
+    }
+return false
+})
 
-
-
-
+console.log(`arr2`, sortNumbers.concat(word));
