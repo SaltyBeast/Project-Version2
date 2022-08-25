a371c2d7362727fe169c86a767823d06e1367ba1
WIN-JJV0OHJOAVK <user@WIN-JJV0OHJOAVK>
2022-08-25 14:25:17 +0300

    Start

--- src/index.js
+++ src/index.js
@@ -1,26 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+const App = <div>
+    Hello world!
+</div>
 
-const mySort = (arr = [], func) => {
-return () => {
-    return arr.sort((value, next) => {
-        return typeof func === 'function'
-        ? func(value, next)
-        : value > next
-        ? 1
-        : value < next
-        ? -1
-        : 0;
-    });
-};
-};   
-const arr = [1, 5, 10, 45, 56]
-const sortFunc1 = mySort([...arr]);
-const sortFunc2 = mySort([...arr], (value, next) => {
-    return value > next
-    ? -1
-    : value < next
-    ? 1
-    : 0;
-});
-
-console.log(sortFunc1(), sortFunc2());
+ReactDOM.render(App, document.getElementById('root'));
