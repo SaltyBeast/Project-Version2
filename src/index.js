6c42411fb06aee0eba71ba7afa50ce6d31f4a7ee
WIN-JJV0OHJOAVK <user@WIN-JJV0OHJOAVK>
2022-08-25 14:07:04 +0300

    hof

--- src/index.js
+++ src/index.js
@@ -1,41 +1,26 @@
 
-const arr =  [
-    110,
-    1,
-    `hello world`,
-    3,
-    4,
-    18,
-    24 
-];
 
-
-const func = (prevValue, nextValue) => {
-    if (prevValue === nextValue) {
-        return 0;
-    }
-    if (prevValue < nextValue) {
-        return -1;
-    }
-    if (prevValue > nextValue) {
-        return 1;
-    }
+const mySort = (arr = [], func) => {
+return () => {
+    return arr.sort((value, next) => {
+        return typeof func === 'function'
+        ? func(value, next)
+        : value > next
+        ? 1
+        : value < next
+        ? -1
+        : 0;
+    });
 };
+};   
+const arr = [1, 5, 10, 45, 56]
+const sortFunc1 = mySort([...arr]);
+const sortFunc2 = mySort([...arr], (value, next) => {
+    return value > next
+    ? -1
+    : value < next
+    ? 1
+    : 0;
+});
 
-const sortNumbers = arr
-.filter((value) => {
-    if (typeof value === 'number') {
-        return true;
-    }
-    return false;
-})
-.sort(func);
-
-const word = arr.filter((value) => {
-    if (typeof value === 'string') {
-    return true;
-    }
-return false
-})
-
-console.log(`arr2`, sortNumbers.concat(word));
+console.log(sortFunc1(), sortFunc2());
